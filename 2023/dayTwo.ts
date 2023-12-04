162ef3839a06f5db2aa0775d4799959750007e6c
Dave Ogle <dogle@scottlogic.com>
2023-12-04 13:19:47 +0000

    Day two part one

--- 2023/dayTwo.ts
+++ 2023/dayTwo.ts
@@ -1,25 +1,63 @@
 // https://adventofcode.com/2023/day/2
 
-import * as fs from 'fs';
+import * as fs from "fs";
 
 function readData(): string[] {
-    const words = fs.readFileSync('data/2-1.txt', 'utf-8');
-    return words.split('\r\n');
+  const words = fs.readFileSync("data/2-1.txt", "utf-8");
+  return words.split("\r\n");
+}
+
+interface Cubes {
+  red: number;
+  green: number;
+  blue: number;
 }
 
 export function dayTwoPartOne() {
-    const bagContents: { red: number, green: number, blue: number } = {
-        red: 12,
-        green: 13,
-        blue: 14
-    };
-    const games = readData().map(line => line.split(":")[1].trim());
+  let result = 0;
+  const bagContents: Cubes = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
+  const games = readData().map((line) => line.split(":")[1].trim());
 
-    const isGamePossible = (game: string): boolean => {
-        const sets = game.split(";")
-        console.log(sets);
-        return true;
+  const isGamePossible = (game: string): boolean => {
+    const sets = game.split(";");
+
+    // sets.forEach(set => {
+    //     const cubes = set.split(",");
+    //     cubes.forEach(cube => {
+    //         const cubeParts = cube.trim().split(" ");
+    //         gameCount[cubeParts[1] as keyof Cubes ]+= Number(cubeParts[0])
+    //     });
+    // })
+
+    return sets.reduce((acc, set) => {
+      const cubes = set.split(",");
+      const setCount: Cubes = {
+        red: 0,
+        green: 0,
+        blue: 0,
+      };
+      cubes.forEach((cube) => {
+        const cubeParts = cube.trim().split(" ");
+        setCount[cubeParts[1] as keyof Cubes] += Number(cubeParts[0]);
+      });
+      return (
+        acc &&
+        setCount.blue <= bagContents.blue &&
+        setCount.green <= bagContents.green &&
+        setCount.red <= bagContents.red
+      );
+    }, true);
+  };
+
+  games.forEach((g, i) => {
+    const gamePossible = isGamePossible(g);
+    if (gamePossible) {
+      result += i + 1;
     }
-
-    games.forEach(g => isGamePossible(g));
+  });
+  console.log(result);
 }
